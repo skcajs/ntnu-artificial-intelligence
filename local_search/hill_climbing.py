b086ae65508ae15f396fff027afc470cc951a81c
samj <sjackson864@outlook.com>
2023-05-10 11:46:13 +0200

    added hill climbing

---
+++ local_search/hill_climbing.py
@@ -0,0 +1,26 @@
+from utils import Problem
+import math
+
+class HillClimbing:
+    def __init__(self, problem: Problem):
+        self.problem = problem
+
+    def search(self) -> int:
+        current = self.problem.initial_state
+        while True:
+            neighbor = self.problem.next_state(current)
+            if self.problem.value(neighbor) <= self.problem.value(current):
+                return current
+            current = neighbor
+            
+if __name__ == "__main__":
+
+    def objective(x: int):
+        return math.sin(x)
+    
+    state = 3
+    problem = Problem(state, step=1, objective=objective)
+    
+    hc = HillClimbing(problem=problem)
+
+    print(hc.search())
---
+++ local_search/utils.py
@@ -0,0 +1,21 @@
+class State:
+    def __init__(self, h = 0):
+        self.h = h
+
+class Problem:
+    def __init__(self, initial_sate: int, successors, heuristic):
+        self.initial_state = initial_sate
+        self.successors = successors
+        self.heuristic = heuristic
+    
+    def next_state(self, state) -> int:
+        top = state+1
+        bottom = state-1
+        if (self.value(top) > self.value(bottom)):
+            return top
+        else:
+            return bottom
+       
+
+    def value(self, state) -> int:
+        return self.heuristic(state)
